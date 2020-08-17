b2a8f2bff357ef36326abcf8c7cb459312466689
tungtvt <trinhvutung87@gmail.com>
2020-08-17 11:46:39 +0700

    Add env.js and env-sample.js file, refactor code

--- src/index.js
+++ src/index.js
@@ -1,46 +1,11 @@
+import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "jquery/dist/jquery.slim";
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'bulma/css/bulma.min.css';
-import axios from 'axios';
-import './index.css';
 import App from './App';
-import Amplify from "aws-amplify";
-import config from "./config";
 import * as serviceWorker from './serviceWorker';
-import { Auth } from 'aws-amplify';
-
-Amplify.configure({
-  Auth: {
-    mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID
-  }
-});
-
-axios.defaults.baseURL = config.base_api;
-// axios.interceptors.request.use(request => {
-//   console.log()
-//   return request;
-// }, error => {
-//   console.log(error)
-// })
-
-axios.interceptors.request.use(function (config) {
-  return Auth.currentSession()
-    .then(session => {
-      // User is logged in. Set auth header on all requests
-      config.headers.Authorization = 'Bearer ' + session.accessToken.jwtToken
-      return Promise.resolve(config)
-    })
-    .catch(() => {
-      // No logged-in user: don't set auth header
-      return Promise.resolve(config)
-    })
-})
-
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,33 +1,32 @@
 import axios from 'axios';
-import config from "../config";
-import { Auth } from 'aws-amplify';
-import Amplify from "aws-amplify";
+import { BE_URL, ENVIRONMENT } from './constants';
+import authService from '../services/auth.service';
 
-//var idToken = localStorage.getItem('CognitoIdentityServiceProvider.7vi3jfs4t8htrbgd9i7dhms6ib.lmdung81.idToken');
-Amplify.configure({
-  Auth: {
-    mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+const api_instance = axios.create({
+  baseURL: BE_URL,
+  timeout: ENVIRONMENT().timeOutApi,
+});
+
+api_instance.interceptors.request.use(
+  (config) => {
+    const token = authService.getAccessToken();
+    const isExpire = authService.isExpire();
+    const isRefresh = authService.isRefresh();
+
+    if (token && !isExpire) {
+      config.headers.authorization = `Bearer ${token}`;
+      if (isRefresh) {
+        authService.getRefreshToken();
+      }
+    } else {
+      // authService.logout();
+      // window.location.href = `/login`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-});
+);
 
-var api_instance = axios.create({
-  baseURL: config.base_api
-  /* other custom settings */
-});
-
-api_instance.interceptors.request.use(function (config) {
-  return Auth.currentSession()
-    .then(session => {
-      // User is logged in. Set auth header on all requests
-      config.headers.Authorization = 'Bearer ' + session.accessToken.jwtToken
-      return Promise.resolve(config)
-    })
-    .catch(() => {
-      // No logged-in user: don't set auth header
-      return Promise.resolve(config)
-    })
-})
 export default api_instance;
